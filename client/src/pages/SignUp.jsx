4ce7ac5a73540f74f7b25107b89955f584d50f91
amruthap16 <jaganccf.123@gmail.com>
2024-05-28 00:05:42 +0530

    adding functionality to the signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,6 +7,7 @@ export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate =useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
@@ -30,7 +31,7 @@ export default function SignUp() {
       }
       setLoading(false);
       if(res.ok){
-        Navigate('/sign-in');
+        navigate('/sign-in');
       }
     }catch (error){
       setErrorMessage(error.message);
