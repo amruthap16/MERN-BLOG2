81e19be509f396e1b621465b523d81f0105149e0
amruthap16 <jaganccf.123@gmail.com>
2024-05-29 20:53:42 +0530

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,3 +1,117 @@
+// import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+// //import { set } from 'mongoose';
+// import React, { useState } from 'react';
+// import { Link, useNavigate} from 'react-router-dom';
+// //import { useDispatch, useSelector } from 'react-redux';
+// //import { SignInStart, SignInSuccess, SignInFailure } from '../redux/user/userSlice';
+
+// export default function SignIn() {
+//   const [formData, setFormData] = useState({});
+//   const [errorMessage, setErrorMessage] = useState(null);
+//   //const {loading, error: errorMessage} = useSelector(stat
+//   const [loading, setLoading] = useState(false);
+//   //const dispatch = useDispatch();
+//   const navigate =useNavigate();
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     if(!formData.email || !formData.password){
+//       //return dispatch(SignInFailure('Please fill all the fields'));
+//       return setErrorMessage('Please fill all the fields');
+//     }
+//     try{
+//       //dispatch(SignInStart());
+//       setLoading(true);
+//       setErrorMessage(null);
+//       const res = await fetch('/api/auth/signin', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(formData),
+//       });
+//       const data = await res.json(); 
+//       if(data.success === false){
+//         //dispatch(SignInFailure(data.message));
+//         return setErrorMessage(data.message);
+//       }
+//       setLoading(false);
+//       if(res.ok){
+//         //dispatch(SignInSuccess(data));
+//         navigate('/');
+//       }
+//     }catch (error){
+//       //dispatch(SignInFailure(error.message));
+//       setErrorMessage(error.message);
+//       setLoading(false);
+//     }
+//   };
+//   return (
+//     <div className='min-h-screen mt-20'>
+//       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+//         {/*left */}
+//         <div className='flex-1'>
+//         <Link to="/" className='font-bold dark:text-white text-4xl'>
+//             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Amrutha's</span>
+//             Blog
+//         </Link>
+//         <p className='text-sm mt-5'>
+//         This is demo project.You can sign in with your email and password or with Google.
+//         </p>
+//         </div>
+//           {/*right */}
+
+//           <div className='flex-1'>
+//             <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+
+//               <div>
+//                 <Label value='Your email' />
+//                 <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}
+//               />
+        
+//               </div>
+//               <div>
+//                 <Label value='Your password' />
+//                 <TextInput type='password' placeholder='********' id='password' onChange={handleChange}
+//               />
+        
+//               </div>
+//               <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+//                {
+//                 loading ? (
+//                   <>
+//                   <Spinner size='sm'/>
+//                   <span className='pl-3'>Loading...</span>
+//                   </>
+                  
+//                 ) : (
+//                   'Sign In' 
+
+//                )}
+//               </Button>
+//              </form> 
+//              <div className="flex gap-2 text-sm mt-5">
+//               <span>
+//               Don't have an account?
+//               </span>
+//               <Link to='/sign-up' className='text-blue-500'>
+//                 Sign Up
+//               </Link>
+//             </div>
+//             {
+//               errorMessage &&(
+//                 <Alert className='mt-5' color='failure'>
+//                   {errorMessage}
+//                 </Alert>
+//               )
+//             }  
+//           </div>
+//       </div>
+//     </div>
+//   )
+// }
+// }
+
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 //import { set } from 'mongoose';
 import React, { useState } from 'react'
@@ -55,7 +169,7 @@ export default function SignIn() {
 
           <div className='flex-1'>
             <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-
+              
               <div>
                 <Label value='Your email' />
                 <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}
@@ -64,7 +178,7 @@ export default function SignIn() {
               </div>
               <div>
                 <Label value='Your password' />
-                <TextInput type='password' placeholder='********' id='password' onChange={handleChange}
+                <TextInput type='password' placeholder='*******' id='password' onChange={handleChange}
               />
         
               </div>
@@ -84,7 +198,7 @@ export default function SignIn() {
              </form> 
              <div className="flex gap-2 text-sm mt-5">
               <span>
-              Don't have an account?
+               Don't Have an account?
               </span>
               <Link to='/sign-up' className='text-blue-500'>
                 Sign Up
@@ -105,4 +219,3 @@ export default function SignIn() {
     </div>
   )
 }
-
