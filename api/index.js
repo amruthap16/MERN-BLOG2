4877ecd2af7d03bc4e9ed160aff31063109c37ea
amruthap16 <jaganccf.123@gmail.com>
2024-06-14 22:04:38 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -35,8 +35,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-
-
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
