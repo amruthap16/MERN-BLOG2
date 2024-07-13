4ea71ffa09276889d24f04bd0431bd577595a6a7
amruthap16 <jaganccf.123@gmail.com>
2024-07-13 05:43:23 +0530

    add pre installation

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,8 +11,7 @@ export default function FooterCom() {
             <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
                 <div className='mt-5'>
                 <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Amrutha's</span>
-            Blog
+            <span className='px-2 py-1 bg-gradient-to-r from-purple-500 via-pink-500 to-blue-300 rounded-lg text-white'>Amrutha's  Blog</span>
         </Link>
                 </div>
                 <div className="grid grid-cols-2 gap-8  mt-4 sm:grid-cols-3 sm:gap-6">
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
+// import React from 'react';
+
+// export default function About() {
+//   // return (
+//   //   // <div className="min-h-screen flex items-center justify-center">
+//   //   //   <div className="max-w-2xl mx-auto p-3 text-center">
+//   //   //     <div>
+//   //   //     <h1 className='text-3xl font font-semibold text-center my-7'>
+//   //   //         About Amrutha's Blog
+//   //   //       </h1>
+//   //   //       <div className="text-md text-gray-500 flex flex-col gap-6">
+//   //   //       <p> Welcome to Amrutha's Blog! This blog was created by Amrutha Pandavula as a personal project to share his thoughts and ideas with the world. Amrutha is a aspiring mern stack developer looking for an oppurtunity as a fresher </p>
+//   //   //         <p> On this blog, you'll find weekly articles and tutorials on topics  such as web development, software engineering, and programming  languages. Amrutha is always learning and exploring new  technologies, so be sure to check back often for new content!</p>
+//   //   //         <p>We encourage you to leave comments on our posts and engage with  other readers. You can like other people's comments and reply to  them as well. We believe that a community of learners can help each other grow and improve. </p>
+//   //   //       </div>
+//   //   //     </div>
+//   //   //   </div>
+//   //   // </div>
+//   // )
+// }
+
+import React from 'react'
 
 export default function About() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="max-w-2xl mx-auto p-3 text-center">
-        <div>
-        <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Amrutha's Blog
-          </h1>
-          <div className="text-md text-gray-500 flex flex-col gap-6">
-          <p> Welcome to Amrutha's Blog! This blog was created by Amrutha Pandavula as a personal project to share his thoughts and ideas with the world. Amrutha is a aspiring mern stack developer looking for an oppurtunity as a fresher </p>
-            <p> On this blog, you'll find weekly articles and tutorials on topics  such as web development, software engineering, and programming  languages. Amrutha is always learning and exploring new  technologies, so be sure to check back often for new content!</p>
-            <p>We encourage you to leave comments on our posts and engage with  other readers. You can like other people's comments and reply to  them as well. We believe that a community of learners can help each other grow and improve. </p>
-          </div>
-        </div>
-      </div>
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        {/* <div className="flex-1 justify-center flex flex-col"> */}
+        <div className="min-h-screen flex items-center justify-center">
+   <div className="max-w-2xl mx-auto p-3 text-center">
+     <div>
+     <h1 className='text-3xl font font-semibold text-center my-7'>
+        About Amrutha's Blog
+       </h1>
+      <div className="text-md text-gray-500 flex flex-col gap-6">
+       <p> Welcome to Amrutha's Blog! This blog was created by Amrutha Pandavula as a personal project to share his thoughts and ideas with the world. Amrutha is a aspiring mern stack developer looking for an oppurtunity as a fresher </p>
+        <p> On this blog, you'll find weekly articles and tutorials on topics  such as web development, software engineering, and programming  languages. Amrutha is always learning and exploring new  technologies, so be sure to check back often for new content!</p>
+         <p>We encourage you to leave comments on our posts and engage with  other readers. You can like other people's comments and reply to  them as well. We believe that a community of learners can help each other grow and improve. </p>
+       </div>
     </div>
+   </div>   
+      </div>
+
+        {/* </div> */}
+      </div>
   )
 }
+
+
